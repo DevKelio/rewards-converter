e0a269d53c2f0ae4cb2e15daf120fecbef5e18f6
Hélio David <keliodavid@gmail.com>
2024-06-04 15:19:01 +0200

    Update test

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -8,23 +8,31 @@ public class RewardValueTests {
     void create_with_cash_value() {
         double cashValue = 100;
         var rewardValue = new RewardValue(cashValue);
-        assertEquals(cashValue, rewardValue.getCashValue());
+        assertEquals(cashValue, rewardValue.getCashValue(), "O valor em dinheiro deve corresponder");
     }
 
     @Test
     void create_with_miles_value() {
         int milesValue = 10000;
         var rewardValue = new RewardValue(milesValue);
-        assertEquals(milesValue, rewardValue.getMilesValue());
+        assertEquals(milesValue, rewardValue.getMilesValue(), "O valor em milhas deve corresponder");
     }
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100;
+        var rewardValue = new RewardValue(cashValue);
+        double expectedMiles = 2000;
+        double actualMiles = rewardValue.convertToMiles();
+        assertEquals(expectedMiles, actualMiles, "A conversão de cash para milhas deve ser precisa");
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 10000;
+        var rewardValue = new RewardValue(milesValue);
+        double expectedCash = 500;
+        double actualCash = rewardValue.convertToCash();
+        assertEquals(expectedCash, actualCash, "A conversão de milhas para cash deve ser precisa");
     }
 }
